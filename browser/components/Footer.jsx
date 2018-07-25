c367d9c939fbe8c858ddeb3604fd6ccd76108d53
Alec Davidson <davidson9076@gmail.com>
2018-07-25 15:58:19 -0400

    add static serving

---
+++ browser/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className="footer text-center">
+    <div className="vcenter muted">
+      <span>
+        <span className="glyphicon glyphicon-wrench" /> by <a href="http://alecdavidson.com">Alec Davidson</a>
+      </span>
+    </div>
+  </div>
+);
+
+export default Footer;
